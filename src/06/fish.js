bec93e6daff597936039875d2e1cbf1744a35f13
Štěpán Śevčík <Thoronir42@Gmail.com>
2021-12-07 23:41:20 +0100

    Adjust 06 lanternfish for phase 2

--- src/06/fish.js
+++ src/06/fish.js
@@ -1,12 +1,16 @@
-import { runTask, streamLines } from "../utils/ioUtils.js";
+import { runTask, streamLines } from "../utils/ioUtils.js"
 
 runTask(async function() {
     const fish = await loadFish()
     this.reportPhase('io')
-    const resultFish = simulateLanternfish(fish, 80)
-    this.reportPhase('algo')
+    
+    let fishCount = estimateLanternFish(fish, 80)
+    this.reportPhase('algo pt1')
+    console.log(fishCount)
 
-    console.log(resultFish.length);
+    fishCount = estimateLanternFish(fish, 256)
+    this.reportPhase('algo pt2')
+    console.log(fishCount)
 })
 
 async function loadFish() {
@@ -17,20 +21,51 @@ async function loadFish() {
     return fish
 }
 
-function simulateLanternfish(fish, duration) {
-    const fishWorkingCopy = fish.slice()
-    const newFish = []
+function estimateLanternFish(fish, duration) {
+    const groups = [...Array.from({length: 9})].map(() => 0)
+    fish.forEach((dtb, i) => {
+        groups[dtb]++
+    })
 
-    for (let i = 0; i < duration; i++) {
-        fishWorkingCopy.forEach((_, i) => {
-            if (--fishWorkingCopy[i] < 0) {
-                newFish.push(8)
-                fishWorkingCopy[i] = 6
-            }
-        })
-
-        fishWorkingCopy.push(...newFish.splice(0))
+    for (let day = 0; day < duration; day++) {
+        const b = groups.shift()
+        groups.push(0)
+        groups[6] += b
+        groups[8] += b
     }
 
-    return fishWorkingCopy
+    return groups.reduce((sum, g) => sum += g, 0)
+}
+
+function simulateLanternfish(fish, duration) {
+    const groups = [...Array.from({length: 9})].map(() => [])
+    fish.forEach((dtb, i) => {
+        groups[dtb].push(i)
+    })
+    let nextId = fish.length
+
+    let tmp = []
+    for (let day = 0; day < duration; day++) {
+        const b = groups.shift()
+        groups.push(tmp)
+
+        b.forEach((id) => {
+            groups[6].push(id)
+            groups[8].push(nextId++)
+        })
+        b.splice(0)
+        tmp = b
+
+        // console.log(reconstructFish(groups).join(','));
+    }
+
+    return groups.reduce((sum, g) => sum += g.length, 0)
+}
+
+function reconstructFish(groups) {
+    const fish = []
+    groups.forEach((g, dtb) => {
+        g.forEach((f) => fish[f] = dtb)
+    })
+    return fish
 }
