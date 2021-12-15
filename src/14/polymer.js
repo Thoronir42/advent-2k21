abda3a5b8f7d34778611f0e9925c944317f6f6d7
Štěpán Śevčík <Thoronir42@Gmail.com>
2021-12-15 11:44:32 +0100

    Adjust 14 polymer before phase 2

--- src/14/polymer.js
+++ src/14/polymer.js
@@ -3,12 +3,10 @@ import LineParser from "../utils/LineParser.js";
 
 runTask(async function() {
     const {chain, insertionRules} = await loadPolymerization()
+    const ctrl = polyMem
 
-    let result = chain
-    for (let i = 1; i <= 10; i++) {
-        result = unfoldPolymer(result, insertionRules)
-    }
-    const stats = analyzePolymer(result)
+    const result = ctrl.unfoldPolymer(chain, insertionRules, 10)
+    const stats = ctrl.analyzePolymer(result)
     console.log(stats.extremes.top[1]  - stats.extremes.low[1]);
 })
 
@@ -34,31 +32,41 @@ async function loadPolymerization() {
     return {chain, insertionRules}
 }
 
-function unfoldPolymer(chain, insertionRules) {
-    for (let i = chain.length - 2; i >= 0; i--) {
-        const pair = chain.substr(i, 2)
-        const insertion = insertionRules[pair]
-        if (insertion) {
-            chain = chain.substr(0, i + 1) + insertion + chain.substr(i + 1)
+const polyMem = {
+    unfoldPolymer(chain, insertionRules, steps) {
+        let result = chain
+        for (let i = 1; i <= steps; i++) {
+            result = this._unfoldPolymerStep(result, insertionRules)
         }
-    }
+        return result
+    },
+    _unfoldPolymerStep(chain, insertionRules) {
+        for (let i = chain.length - 2; i >= 0; i--) {
+            const pair = chain.substr(i, 2)
+            const insertion = insertionRules[pair]
+            if (insertion) {
+                chain = chain.substr(0, i + 1) + insertion + chain.substr(i + 1)
+            }
+        }
+    
+        return chain
+    },
 
-    return chain
-}
-function analyzePolymer(chain) {
-    const counts = {}
-    for (let i = 0; i < chain.length; i++) {
-        const c = chain.charAt(i)
-        counts[c] = (counts[c] ?? 0) + 1
-    }
-
-    const countEntries = Object.entries(counts)
-    countEntries.sort((a, b) => b[1] - a[1])
-    const top = countEntries[0]
-    const low = countEntries[countEntries.length - 1]
-
-    return {
-        countsOrdered: countEntries,
-        extremes: {top, low},
+    analyzePolymer(chain) {
+        const counts = {}
+        for (let i = 0; i < chain.length; i++) {
+            const c = chain.charAt(i)
+            counts[c] = (counts[c] ?? 0) + 1
+        }
+    
+        const countEntries = Object.entries(counts)
+        countEntries.sort((a, b) => b[1] - a[1])
+        const top = countEntries[0]
+        const low = countEntries[countEntries.length - 1]
+    
+        return {
+            countsOrdered: countEntries,
+            extremes: {top, low},
+        }
     }
 }
